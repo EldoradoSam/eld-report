630e5647654c7bec59c2a689ba6f7c2e592dda98
EldoradoSam <59946813+EldoradoSam@users.noreply.github.com>
2023-06-16 23:09:42 -0700

    0.1

---
+++ src/RbsReportController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace eld\report;
+
+use Illuminate\Http\Request;
+USE App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\App;
+
+class RbsReportController extends Controller
+{
+    //
+
+    public function showReport(){
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML('<h1>Report</h1>');
+        return $pdf->stream();
+    }
+}
